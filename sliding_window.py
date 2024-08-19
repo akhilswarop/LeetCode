54f03018dce50dab2f06362af47a05a063375d57
Akhil Swarop S <96727867+akhilswarop@users.noreply.github.com>
2024-08-19 13:14:11 +0530

    Refresh

--- sliding_window.py
+++
@@ -1,9 +0,0 @@
-lst = [1,2,3,4,5,6,7,8] 
-def sliding_window(elements, window_size):
-    
-    if len(elements) <= window_size:
-       return elements
-    iterations = len(elements)- window_size + 1
-    for i in range(iterations):
-        print(elements[i:i+window_size])
-sliding_window(lst, 3)
